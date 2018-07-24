98c8b465217652a82391c2c472be8ea3231b7bc0
uehara <b1015062@fun.ac.jp>
2018-07-24 17:40:27 +0900

    version up

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='MeCabOnigiri',
-      version='0.1.0',
+      version='0.1.2',
       description='Python Distribution Utilities',
       author='uehara1414',
       author_email='akiya.noface@gmail.com',
